d1afc0b48613fbc7011df5e63a056fb52f4805df
Joachim <Joachim@Joachim-Pc-2014.home>
2016-10-15 12:17:27 +0200

    Additional Setup added, testing still in progress.

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,36 +1,36 @@
-(function(){
+(function () {
     var app = angular.module("routingApp", ["ngRoute"]);
-    app.config(function($routeProvider) {
+    app.config(function ($routeProvider) {
         $routeProvider
-        .when("/all", {
-            templateUrl: "allPeople.html",
-            controller: "UserController as userCtrl"
-        })
-        .when("/detail", {
-            templateUrl: "personDetails.html",
-            controller: "UserController as userCtrl"
-        })
-        .otherwise({
-            templateUrl : "index.html"
-        });
+                .when("/all", {
+                    templateUrl: "allPeople.html",
+                    controller: "UserController",
+                    controllerAs: "userCtrl"
+                })
+                .when("/detail", {
+                    templateUrl: "personDetails.html",
+                    controller: "UserController",
+                    controllerAs: "userCtrl"
+                })
+                .otherwise({
+                    template: "<p>Nothing has been selected,</p>"
+                });
     });
-    var users=[];
-    app.controller("UserController", function($http, $routeParams) { 
+    var users = [];
+    app.controller("UserController", function ($http, $routeParams) {
         var self = this;
-        var person;
         if (users.length === 0) {
             $http.get("data/data.json").success(function (data) {
                 users = data.users;
                 self.users = users;
             });
+        } else { //We used the cache property on the http request instead
+            self.users = users;
         }
-        else{ //We used the cache property on the http request instead
-            self.users = users; 
-        } 
-            if (users !== null) {
-                console.log("Adding user: "+$routeParams.id);
-                self.user = users[$routeParams.id];
-            }
-        
+        if (users !== null) {
+            console.log("Adding user: " + $routeParams.id);
+            self.user = users[$routeParams.id];
+        }
+
     });
 });
